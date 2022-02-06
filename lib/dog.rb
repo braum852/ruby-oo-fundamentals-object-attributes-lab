1ffb5b1d622de9f4378b67236d012a5db016603b
braum852 <julian.teo59@yahoo.com>
2022-02-06 11:38:30 +1000

    Done

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class Dog
+attr_accessor :name, :breed
+##Task is create variable named 'name' and 'breed' that
+##writes and reads the name and breed from an instance variable
+def initialize
+      end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+    attr_accessor :name, :job
+    ##Task is create variable named 'name' and 'job' that
+    ##writes and reads the name and job from an instance variable
+    def initialize
+          end
+    
+    end
